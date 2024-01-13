1ef19b848a7cadb9753fdaf7f80751774bb1c2e9
Eduardo Bianchi <134458207+bianchi-ed@users.noreply.github.com>
2024-01-13 05:39:55 -0300

    base structure

--- twitchIRC/twitch.js
+++ twitchIRC/twitch.js
@@ -1,5 +1,5 @@
 const { Client: TwitchClient } = require('@twitchapis/twitch.js');
-const {twitchUserToken, targetChannel } = require('../config.json');
+const { targetChannel } = require('../config.json');
 
 // Instance new twitch.js client
 const twitchJsClient = new TwitchClient({
@@ -17,10 +17,7 @@ twitchJsClient.on('message', (msg) => {
 		msg.channel.send('pong');
 	}
 
-	console.log(msg.content);
+	console.log(msg.author.displayName + ": " + msg.content);
 });
-
-// IRC chat login using twitch user token
-twitchJsClient.login(twitchUserToken);
 
 module.exports = twitchJsClient;
